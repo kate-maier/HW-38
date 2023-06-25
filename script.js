217088dc97a6eebfa792f6e3d80951ea155ede9b
AdminK <volvenkova.k@gmail.com>
2023-06-25 10:46:21 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// Написати приклад використання методів функцій window.open()/window.close().
+
+
+let screenWidth = window.screen.width;
+let screenHeight = window.screen.height;
+let windowWidth = 400;
+let windowHeight = 500;
+let topPosition = screenHeight - windowHeight;
+let leftPosition = screenWidth - windowWidth;
+
+let openedWindow;
+
+showWindowBtn.onclick = () => {
+  openedWindow = window
+    .open('https://ithillel.ua/', 'hillel', `top=${topPosition}, left=${leftPosition}, width=${windowWidth}, height=${windowHeight}`);
+};
+
+
+closeWindowBtn.onclick = () => {
+  openedWindow.close();
+}
+
+closePage.onclick = () => {
+  window.close();
+}
